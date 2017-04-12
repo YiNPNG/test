6db4ae45b7536ab7b84ad8c6d42e475868bf8c33
YiNPNG <1020379110@qq.com>
2017-04-12 17:52:42 +0800

    Dialog

---
+++ MyAlertDialog/app/src/main/java/com/example/dell/myalertdialog/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.dell.myalertdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setIcon(android.R.drawable.ic_dialog_info);
+        dialog.setTitle("欢迎");
+        dialog.setMessage("欢迎使用本程序");
+        dialog.setPositiveButton("肯定", new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        dialog.setNegativeButton("否定", new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        dialog.create();
+        dialog.show();
+    }
+}
